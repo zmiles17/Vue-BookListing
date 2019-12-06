339e7a7096ed5e58e7376e7806814c892f4baecb
zmiles17 <zgm_striker17@hotmail.com>
2019-12-05 20:16:09 -0500

    Complete Module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1> {{ title }} </h1>
+        <ul>
+            <li v-for="book in books">{{ book.title }}:{{ book.author }}</li>
+        </ul>
     </div>
 </template>
 
@@ -9,14 +12,31 @@
         name: "BookList",
         data: function() {
             return {
-                title: "All Books"
+                title: "All Books",
+                books: [
+    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Gods', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+]
             }
         }
     }
 </script>
 
 <style>
+
     h1, h2 {
         font-weight: 'normal';
     }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+
 </style>
