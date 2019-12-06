29175c2525fbccde07e458e78f76b1379db16e4e
zmiles17 <zgm_striker17@hotmail.com>
2019-12-05 22:43:09 -0500

    m5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,24 +2,36 @@
     <div>
         <h1> {{ title }} </h1>
         <ul>
-            <book-item v-for="book in books" :book=book></book-item>
+            <book-item v-for="book in books" :book='book' v-bind:key='books.indexOf(book)'></book-item>
         </ul>
+        <hr>
+        <book-form @addBook='appendBook'></book-form>
     </div>
 </template>
 
 <script>
     import BookItem from './BookItem';
+    import BookForm from './BookForm';
+
     export default {
         name: "BookList",
-        data: function() {
+        components: {
+            BookItem,
+            BookForm
+        },
+        data() {
             return {
                 title: "All Books",
                 books: [
                     {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
                     {title: 'American Gods', author: 'Neil Gaiman'},
                     {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-                ],
-                components: {BookItem}
+                ]
+            }
+        },
+        methods: {
+            appendBook(bookTitle, bookAuthor) {
+                this.books.push({ title: bookTitle, author: bookAuthor });
             }
         }
     }
